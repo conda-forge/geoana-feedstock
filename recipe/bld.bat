46da9c432901583b4c4124959c15319d9a1f220b
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-11-20 14:51:43 -0800

    try defining M_PI in CPPFLAGS for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
 set "BUILD_GEOANA_EXT=1"
 
+if defined CPPFLAGS (set CPPFLAGS=%CPPFLAGS% \DM_PI=3.14159265358979323846) else (set CPPFLAGS=\DM_PI=3.14159265358979323846)
+
 %PYTHON% -m pip install . --no-deps -vv
