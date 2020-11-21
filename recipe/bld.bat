b7dda840b5af3e6cee30de219bf861d0ef40eeca
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-11-20 20:40:13 -0800

    make script prepend cpp file with _USE_MATH_DEFINES (temporary fix until next geoana release)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,8 @@
 set "BUILD_GEOANA_EXT=1"
 
-echo %CPPFLAGS%
-
-set "CPPFLAGS=/DM_PI=3.14159265358979323846"
+echo #define _USE_MATH_DEFINES > temp.txt
+type geoana\kernels\_extensions\_rTE.cpp >> temp.txt
+type temp.txt > geoana\kernels\_extensions\_rTE.cpp
+del temp.txt
 
 %PYTHON% -m pip install . --no-deps -vv
