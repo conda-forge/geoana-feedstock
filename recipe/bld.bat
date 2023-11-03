bbf25fb1d10d2a71c9ce0ef5340365098560da1f
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-11-03 12:10:34 -0600

    bld.bat update

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,4 @@
 
 %PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Csetup-args="-Dwith_extensions=true" -Csetup-args="--vsenv"
 
-FOR %i in (dist\geoana*.whl) DO %PYTHON% -m pip install %i
+FOR %%i in (dist\geoana*.whl) DO %PYTHON% -m pip install %%i
