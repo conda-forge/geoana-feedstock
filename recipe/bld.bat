e430d485fa3cdefb0d865e8fdc1a050725fcb3e1
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-11-03 11:18:36 -0600

    fix windows path seperator

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,4 @@
 
 %PYTHON% -m build --wheel --no-isolation --skip-dependency-check -Csetup-args="-Dwith_extensions=true" -Csetup-args="--vsenv"
 
-%PYTHON% -m pip install dist/geoana*.whl
+%PYTHON% -m pip install dist\geoana*.whl
