e1d45e7cc3d57d6e5f964803bb6f3720355f2e48
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-11-03 10:34:19 -0600

    create builddir directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,7 @@
 @echo on
 
-meson setup ${MESON_ARGS} -Dwith_extensions=true --vsenv builddir/
+mkdir builddir
+
+meson setup ${MESON_ARGS} -Dwith_extensions=true --vsenv builddir
 
 %PYTHON% -m pip install --no-deps -vv --no-build-isolation  --config-settings=builddir="builddir" .
